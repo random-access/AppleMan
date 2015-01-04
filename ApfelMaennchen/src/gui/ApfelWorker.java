1242ce7384e88b5e670d1565405a9e85b8f96bcd
Peter Petrel <chunkylover@web.de>
2015-01-04 15:31:36 +0100

    Fixed coordinate translation issue. Added a screenshot with calculation.

--- ApfelMaennchen/src/gui/ApfelWorker.java
+++ ApfelMaennchen/src/gui/ApfelWorker.java
@@ -4,42 +4,50 @@ public class ApfelWorker {
 
 	double radius;
 	int maxiter;
-	double [][] values;
+	double[][] values;
 
 	public ApfelWorker(double radius, int maxiter) {
 		this.radius = radius;
 		this.maxiter = maxiter;
 	}
-	
-	private double [] calculateSquare (double x,double y) {
-		return new double[] { x*x-y*y , 2*x*y };	
+
+	private double[] calculateSquare(double x, double y) {
+		return new double[] { x * x - y * y, 2 * x * y };
 	}
-	private double [] calculatePoint (double x,double y, double xc, double yc) {
-		double [] sq = calculateSquare(x,y);
-		return new double[] {sq[0]+xc, sq[1] + yc};	
+
+	private double[] calculatePoint(double x, double y, double xc, double yc) {
+		double[] sq = calculateSquare(x, y);
+		return new double[] { sq[0] + xc, sq[1] + yc };
 	}
-	
-	private double calculateDistance(double xstart,double ystart, double xend, double yend) {
-		return Math.sqrt(Math.pow(xstart-xend, 2) + Math.pow(ystart-yend, 2));
+
+	private double calculateDistance(double xstart, double ystart, double xend,
+			double yend) {
+		return Math.sqrt(Math.pow(xstart - xend, 2)
+				+ Math.pow(ystart - yend, 2));
 	}
-	private int iterateFunction (double x, double y) {
-		double [] start = new double[] {x,y};
-		double [] p     = new double[] {x,y};
-		
-		int iter =0;
-		while (iter < maxiter && radius >= calculateDistance (start[0], start[1], p[0], p[1])) {
-			p=calculatePoint(p[0], p[1], start[0], start[1]);
+
+	private int iterateFunction(double x, double y) {
+		double[] start = new double[] { x, y };
+		double[] p = new double[] { x, y };
+
+		int iter = 0;
+		while (iter < maxiter
+				&& radius >= calculateDistance(start[0], start[1], p[0], p[1])) {
+			p = calculatePoint(p[0], p[1], start[0], start[1]);
 			iter++;
 		}
-		
+
 		return iter;
 	}
-	public void calculateArray(int [][] screenPoints, double xmax, double ymax, double xmin, double ymin) {
+
+	public void calculateArray(int[][] screenPoints, double xmax, double ymax,
+			double xmin, double ymin) {
 		for (int x = 0; x < screenPoints.length; x++) {
 			for (int y = 0; y < screenPoints[0].length; y++) {
-				screenPoints[x][y] = iterateFunction((-1) * (double)x*(xmax-xmin)/screenPoints.length-(xmax+xmin) , (double)y*(ymax-ymin)/screenPoints[0].length - (ymax+ymin));
+				screenPoints[x][y] = iterateFunction((double) x * (xmax - xmin)
+						/ screenPoints.length + (xmin), (double) (-1) * y
+						* (ymax - ymin) / screenPoints[0].length + (ymin));
 			}
 		}
 	}
-
 }
--- ApfelMaennchen/src/gui/PaintPanel.java
+++ ApfelMaennchen/src/gui/PaintPanel.java
@@ -1,45 +1,45 @@
 package gui;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class PaintPanel extends JPanel {
-	int [][] points;
-	
-	
-	int width  = 800;
+	int[][] points;
+
+	int width = 800;
 	int height = 600;
 
-	
 	public PaintPanel() {
 		ApfelWorker worker = new ApfelWorker(10.0, 500);
 		points = new int[width][height];
-		worker.calculateArray(points, 0, 0, -2, -2);
-		
+		worker.calculateArray(points, 0.5, 2, -0.5, 1);
+
 		setPreferredSize(new Dimension(width, height));
 		setBackground(Color.WHITE);
 		setOpaque(true);
-		
+
 	}
-	
+
 	protected void paintComponent(Graphics g) {
 		System.out.println("in paintComponent");
-		super.paintComponent(g);		
-		Graphics2D g2d = (Graphics2D)g;
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
 		drawPoints(g2d);
 
 	}
-	
+
 	private void drawPoints(Graphics2D g2d) {
 		for (int x = 0; x < points.length; x++) {
-			for(int y = 0; y < points[0].length; y++) {
-				g2d.setColor(new Color((points[x][y]*10) % 125, 0, 0));
+			for (int y = 0; y < points[0].length; y++) {
+				g2d.setColor(new Color((points[x][y] * 10) % 125, 0, 0));
 				g2d.drawLine(x, y, x, y);
 			}
 		}
 	}
-	
-	
+
 }
